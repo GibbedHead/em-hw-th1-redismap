034a02a88d630759e84e3a7e00a1acdfe5c7a8e9
GibbedHead <chaplygin-ma@yandex.ru>
2024-10-09 21:38:30 +0300

    В тестовом модуле в Main.main написал примеры использования.

--- redismapdemo/src/main/java/ru/chaplyginma/Main.java
+++ redismapdemo/src/main/java/ru/chaplyginma/Main.java
@@ -1,30 +1,47 @@
 package ru.chaplyginma;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 import ru.chaplyginma.redismap.map.RedisMap;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         Jedis jedis = new Jedis();
-        RedisMap map = new RedisMap(jedis);
+        RedisMap redisMap = new RedisMap(jedis);
 
-        map.put("k1", "v1");
-        map.put("k2", "v2");
-        map.put("k3", "v3");
+        logger.info("Adding 3 entries to redis map");
+        redisMap.put("key1", "value1");
+        redisMap.put("key2", "value2");
+        redisMap.put("key3", "value3");
 
-        System.out.println(map.get("k1"));
+        logger.info("for 'key1' value is '{}'", redisMap.get("key1"));
 
-        map.remove("k1");
+        logger.info("Adding map to redis map");
+        Map<String, String> toAdd = new HashMap<>();
+        toAdd.put("toAdd_key1", "toAdd_value1");
+        toAdd.put("toAdd_key2", "toAdd_value2");
+        redisMap.putAll(toAdd);
+        logger.info("Getting val for added map key 'toAdd_key1': {}", redisMap.get("toAdd_key1"));
 
-        System.out.println(map.get("k1"));
+        logger.info("New map size: {}", redisMap.size());
 
-        System.out.println(map.keySet());
-        System.out.println(map.values());
-        System.out.println(map.entrySet());
+        logger.info("Removing key 'toAdd_key2' from redis map");
+        redisMap.remove("toAdd_key2");
+        logger.info("After removing key 'toAdd_key2' from redis map new size: {}", redisMap.size());
 
-        System.out.println(map.containsKey("k1"));
-        System.out.println(map.containsKey("k2"));
+        logger.info("Is map empty? '{}'", redisMap.isEmpty());
 
+        logger.info("Keys of a map are: {}", redisMap.keySet());
+        logger.info("Values of a map are: {}", redisMap.values());
 
+        logger.info("Does map contains key 'toAdd_key1'? '{}'", redisMap.containsKey("toAdd_key1"));
+        logger.info("Does map contains value 'toAdd_value1'? '{}'", redisMap.containsValue("toAdd_value1"));
     }
 }
